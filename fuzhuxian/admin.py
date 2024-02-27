89c05988bc4345b686890bb60c5a232d378567f6
owen125717 <1257173018@qq.com>
2024-02-27 13:25:14 +0800

    添加admin后台的id显示

--- fuzhuxian/admin.py
+++ fuzhuxian/admin.py
@@ -42,14 +42,14 @@ class ImageInline(admin.TabularInline): # 可以改成 admin.StackedInline，如
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name',)  # 在Admin列表中显示的字段
+    list_display = ('id', 'name',)  # 在Admin列表中显示的字段
     search_fields = ('name',)  # 在Admin中搜索的字段
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     inlines = [ImageInline,]  # 添加inline属性
     fields = ('title', 'body', 'author',  'tags','status')
-    list_display = ('title', 'body', 'author', 'status','created_at')
+    list_display = ('id', 'title', 'body', 'author', 'status','created_at')
     search_fields = ('title','body',)
     list_filter = ('created_at','author', 'tags','status')  # 右侧筛选栏
     date_hierarchy = 'created_at'  # 顶部时间导航栏
@@ -59,11 +59,11 @@ class PostAdmin(admin.ModelAdmin):
 class CommentAdmin(admin.ModelAdmin):
     inlines = [ImageInline,]  # 添加inline属性相同
     fields = ('body', 'author', 'post')
-    list_display = ('body', 'created_at', 'author', 'post', 'created_at')
+    list_display = ('id', 'body', 'created_at', 'author', 'post', 'created_at')
     search_fields = ('body',)
     list_filter = ('created_at','author', 'post')
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('image', 'post', 'comment')
+    list_display = ('id', 'image', 'post', 'comment')
